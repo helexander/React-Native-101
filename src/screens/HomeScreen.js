5b9bce846ec90e95082be131a1562160f5386893
helexander <jeremychee97@gmail.com>
2021-07-29 12:52:41 +1000

    Added prop and navigation abilities

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+// Rather than passing the entire props object, we can just refactor the code and only use the "navigation" property from props 
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={ styles.text }>HII</Text>
+      {/* Two methods to navigate from one screen to another
+      However, "Button" is less powerful and has less configuration abilities as compared to "TouchableOpacity" 
+      Button is also a self closing tag as compared to TouchableOpacity*/}
       <Button
         title="Go to components demo"
-        onPress={ () => { console.log('Button Pressed') } }
+        onPress={ () => { navigation.navigate('Components') } }
       />
-      <TouchableOpacity onPress={ () => { console.log('List Pressed') } }>
+      <TouchableOpacity onPress={ () => { navigation.navigate('List') } }>
         <Text>Go to list demo</Text>
       </TouchableOpacity>
     </View>
